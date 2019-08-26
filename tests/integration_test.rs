a0814b672d8223ebe6b4f3020a783a7e7c5557f1
Patrick Kerrigan <patrick@patrickkerrigan.uk>
2019-08-26 16:21:16 +0000

    Add first integration test

---
+++ tests/integration_test.rs
@@ -0,0 +1,10 @@
+use std::fs::read_to_string;
+use rust_cnc::{process};
+
+#[test]
+fn dxf_text_test() {
+    let dxf = read_to_string("tests/data/text.dxf").unwrap();
+    let gcode = read_to_string("tests/data/text.nc").unwrap();
+
+    assert_eq!(gcode.trim_end(), process(&dxf));
+}
