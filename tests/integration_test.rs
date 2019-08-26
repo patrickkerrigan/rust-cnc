4399c2593cf087c90505e59f5cfa0f9384eed95a
Patrick Kerrigan <patrick@patrickkerrigan.uk>
2019-08-26 17:21:00 +0000

    Refactor tests a bit

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,18 +1,21 @@
 use std::fs::read_to_string;
 use rust_cnc::{process};
 
+const DATA_PATH: &str = "tests/data/";
+
 #[test]
 fn dxf_shape_test() {
-    let dxf = read_to_string("tests/data/shapes.dxf").unwrap();
-    let gcode = read_to_string("tests/data/shapes.nc").unwrap();
-
-    assert_eq!(gcode.trim_end(), process(&dxf));
+    test_dxf("shapes.dxf", "shapes.nc");
 }
 
 #[test]
 fn dxf_text_test() {
-    let dxf = read_to_string("tests/data/text.dxf").unwrap();
-    let gcode = read_to_string("tests/data/text.nc").unwrap();
+    test_dxf("text.dxf", "text.nc");
+}
 
-    assert_eq!(gcode.trim_end(), process(&dxf));
+fn test_dxf(dxf_file: &str, nc_file: &str) {
+    let dxf = read_to_string(DATA_PATH.to_owned() + dxf_file).unwrap();
+    let gcode = read_to_string(DATA_PATH.to_owned() + nc_file).unwrap();
+
+    assert_eq!(gcode.trim_end(), process(&dxf).trim_end());
 }
