c1e713bddb9f6c58ad6fd2ff3379613408d99158
Patrick Kerrigan <patrick@patrickkerrigan.uk>
2019-08-26 16:44:17 +0000

    Add test for basic shapes

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,6 +2,14 @@ use std::fs::read_to_string;
 use rust_cnc::{process};
 
 #[test]
+fn dxf_shape_test() {
+    let dxf = read_to_string("tests/data/shapes.dxf").unwrap();
+    let gcode = read_to_string("tests/data/shapes.nc").unwrap();
+
+    assert_eq!(gcode.trim_end(), process(&dxf));
+}
+
+#[test]
 fn dxf_text_test() {
     let dxf = read_to_string("tests/data/text.dxf").unwrap();
     let gcode = read_to_string("tests/data/text.nc").unwrap();
